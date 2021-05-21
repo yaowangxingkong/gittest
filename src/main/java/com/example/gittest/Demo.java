59a1ba97417721f59f9525a0a011019165e2b680
chy <chy@chy.com>
2021-05-21 13:41:42 +0800

    v1

--- src/main/java/com/example/gittest/Demo.java
+++ src/main/java/com/example/gittest/Demo.java
@@ -3,6 +3,8 @@ package com.example.gittest;
 public class Demo {
     public void test01(){
         System.out.println("hot-fix");
+        System.out.println("Master");
+        System.out.println("hot-fix2");
 
     }
 }
